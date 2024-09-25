fb69baf5d0ff4dc7a6c40532270ecf67670ef0f2
jihenbo <jihenebo58@gmail.com>
2024-09-25 11:55:58 +0200

    changement effctué

--- db.js
+++ db.js
@@ -2,12 +2,13 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  '', // Database name
-  '', // User
-  '', // Password
+  process.env.DATABASE_Name, // Database name
+  process.env.DATABASE_user, // User
+  process.env.DATABASE_pwd, // Password on==, on va utiliser des variable d'environements
   {
-    host: '', // Host
+    host: 'dpg-crpshmt6l47c73aoti60-a', // Host
     dialect: 'postgres',
+    port:5432,
     dialectOptions: {
       ssl: {
         require: true,
@@ -22,6 +23,14 @@ const sequelize = new Sequelize(
 )
 
 sequelize.authenticate()
+// j ai ajouté ca
+.then(() => {
+  console.log('Connection has been established successfully.');
+})
+.catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
+// jsk ici
 sequelize.sync()
 
 module.exports = sequelize
